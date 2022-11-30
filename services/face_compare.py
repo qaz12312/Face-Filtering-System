d212cb1f6610a7c596f38fb670ea3afcbd2f7996
hedy881028 <hedy881028.cs07@nctu.edu.tw>
2022-11-30 10:59:14 +0800

    fix: read image

--- services/face_compare.py
+++ services/face_compare.py
@@ -1,7 +1,6 @@
 from deepface import DeepFace
 import cv2
 import numpy as np
-import pandas as pd
 
 # remember to modify here!
 test_img = 'image we want to test'
@@ -10,6 +9,7 @@ df = DeepFace.find(img_path=test_img, db_path='db', enforce_detection=False)
 df = df.sort_values(by=['VGG-Face_cosine'], ascending=False) # sort the values from the highest similarity to the lowest
 df = df.reset_index(drop=True)
 
+test_img = cv2.imread(test_img)
 show = 10 # how many images to show
 
 for i in range(show):
