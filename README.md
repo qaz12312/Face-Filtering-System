2c95c83d581ea06260c40d762b2f59b57d7f2dd4
qaz12312 <watermoo22@gmail.com>
2022-12-09 01:55:51 +0800

    Feat: new gui for facial selection

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 + `main.py` 進入點
 
 ## Features
-+ `.\services\ui.py` 介面
 + `.\controller\gui.py` UI 的 controller
++ `.\services\ui.py` 介面
 + `.\services\face_obtain.py` 提取臉部並存進 dataset (人臉偵測與辨識: https://colab.research.google.com/drive/1LOlYpzbhoYSLAtIHMmnjrED55hmrM87_)
 + `.\services\face_compare.py` 比對臉部特徵
     + 需要的 pickle 檔：https://drive.google.com/drive/u/1/folders/1DW4skGaJ0sK6sPp-O1AwAIcJqTX6wE2W
--- controller/gui.py
+++ controller/gui.py
@@ -10,36 +10,71 @@ class GUIController(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(GUIController, self).__init__()
         self.setupUi(self)
-        self.bind_click_events()
-        self.select_sex = None
-        self.select_count = None
+        self.bind_events()
+        self.current_page = 0 # similarity:0, facial:1
+        self.select_sex = [None, None] # for [similarity, facial]
+        self.select_count = [None, None] # for [similarity, facial]
+        # for similarity
         self.target_img_path = None
+        # for  facial
+        self.facial = None
+        self.eyebrow = None
+        self.eye = None
+        self.nose = None
+        self.mouth = None
         self.face_detector = FaceController()
 
-    def bind_click_events(self) -> None:
-        self.radioBtn_woman.clicked.connect(self.click_checked)
-        self.radioBtn_man.clicked.connect(self.click_checked)
-        self.radioBtn_all.clicked.connect(self.click_checked)
-        self.pushBtn_loadImg.clicked.connect(self.click_load_image)
+    def bind_events(self) -> None:
+        self.similarity_action.triggered.connect(self.change_page)
+        self.facial_selection_action.triggered.connect(self.change_page)
+        # similarity page
+        self.radioBtn_woman.clicked.connect(self.click_sex_checked)
+        self.radioBtn_man.clicked.connect(self.click_sex_checked)
+        self.radioBtn_all.clicked.connect(self.click_sex_checked)
+        self.pushBtn_load_img.clicked.connect(self.click_load_target_img)
         self.commandLinkButton_send.clicked.connect(self.click_send)
         self.commandLinkButton_export.clicked.connect(self.click_export)
-    
-    def click_checked(self) -> None:
+        # facial page
+        self.radioBtn_woman_facial.clicked.connect(self.click_sex_checked)
+        self.radioBtn_man_facial.clicked.connect(self.click_sex_checked)
+        self.radioBtn_all_facial.clicked.connect(self.click_sex_checked)
+        self.commandLinkButton_send_facial.clicked.connect(self.click_send)
+        self.commandLinkButton_export_facial.clicked.connect(self.click_export)
+
+    def change_page(self) -> None:
+        menu_name = self.sender().objectName()
+        go_page_idx = -1
+        if menu_name == 'similarity_action':
+            go_page_idx = 0
+        elif menu_name == 'facial_selection_action':
+            go_page_idx = 1
+        else:
+            return
+        self.stackedWidget.setCurrentIndex(go_page_idx)
+        self.current_page = go_page_idx
+
+    def click_sex_checked(self) -> None:
+        """
+        讀取選擇的性別
+        """
         btn_name = self.sender().objectName()
         sex = None
-        if btn_name == 'radioBtn_woman' and self.radioBtn_woman.isChecked():
+        if (btn_name == 'radioBtn_woman' and self.radioBtn_woman.isChecked()) or (btn_name == 'radioBtn_woman_facial' and self.radioBtn_woman_facial.isChecked()):
             sex = 0
-        elif btn_name == 'radioBtn_man' and self.radioBtn_man.isChecked():
+        elif (btn_name == 'radioBtn_man' and self.radioBtn_man.isChecked()) or (btn_name == 'radioBtn_man_facial' and self.radioBtn_man_facial.isChecked()):
             sex = 1
-        elif btn_name == 'radioBtn_all' and self.radioBtn_all.isChecked():
+        elif (btn_name == 'radioBtn_all' and self.radioBtn_all.isChecked()) or (btn_name == 'radioBtn_all_facial' and self.radioBtn_all_facial.isChecked()):
             sex = 2
         else:
             return
-        self.select_sex = sex
-        print(f'click {self.sender().text()} / {self.select_sex}')
+        self.select_sex[self.current_page] = sex
+        print(f'click {self.sender().text()} / {self.select_sex[self.current_page]}')
     
-    def click_load_image(self) -> None:
-        img_path, filter_type = QtWidgets.QFileDialog.getOpenFileName(self, "選擇目標圖像",os.getcwd(),"Image Files (*.png *.xpm *.jpg)")
+    def click_load_target_img(self) -> None:
+        """
+        讀取選擇的目標圖像
+        """
+        img_path, filter_type = QtWidgets.QFileDialog.getOpenFileName(self, "選擇目標圖像", os.getcwd(), "Image Files (*.png *.xpm *.jpg)")
         if len(img_path) == 0 :
             QtWidgets.QMessageBox.about(self, "注意!", "您尚未選擇目標圖像")
         else:
@@ -48,35 +83,72 @@ class GUIController(QtWidgets.QMainWindow, Ui_MainWindow):
             target_img = cv2.imread(self.target_img_path)
             height, width, channel = target_img.shape
             bytes_per_line = 3 * width
-            self.label_showImg.setPixmap(QPixmap.fromImage(QImage(target_img.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()))
-
-    def click_send(self) -> None:
+            self.label_show_img.setPixmap(QPixmap.fromImage(QImage(target_img.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()))
+    
+    def _send_similarity_requires(self) -> list:
+        """
+        傳送人臉相似度的需求
+        """
         # sex
-        if self.select_sex is None :
-            QtWidgets.QMessageBox.about(self, "尚未選擇性別", "請先選擇目標性別，再執行此動作")
-            return
+        if self.select_sex[0] is None :
+            return {'errTitle': "尚未選擇性別",'errMsg': "請先選擇目標性別，再執行此動作"}
         # img
         if self.target_img_path is None :
-            QtWidgets.QMessageBox.about(self, "尚未載入圖片", "請先載入目標圖像，再執行此動作")
-            return
+            return {'errTitle': "尚未載入圖片",'errMsg': "請先載入目標圖像，再執行此動作"}
         # img success
-        isSuccess, msg = self.face_detector.extract_face(self.target_img_path)
-        if isSuccess:
-            self.select_count = int(self.select_resultCount.currentText())
-            print(f'Send infomation:\n\tselect_sex={self.select_sex}\n\tselect_count={self.select_count}\n\timg_source={self.target_img_path}')
-            results = self.face_detector.find_similar_face(self.select_sex, self.select_count, msg)
+        is_success, msg = self.face_detector.extract_face(self.target_img_path)
+        if is_success:
+            self.select_count[0] = int(self.select_result_count.currentText())
+            print(f'Send infomation:\n\tselect_sex={self.select_sex[0]}\n\tselect_count={self.select_count[0]}\n\timg_source={self.target_img_path}')
+            return self.face_detector.find_similar_faces(self.select_sex[0], self.select_count[0], msg)
         else:
-            QtWidgets.QMessageBox.about(self, "請載入其他圖片", msg)
+            return {'errTitle': "請載入其他圖片",'errMsg': msg}
 
+    def _send_facial_selection_requires(self) -> list:
+        """
+        傳送人臉過濾的需求
+        """
+        # sex
+        if self.select_sex[1] is None :
+            return {'errTitle': "尚未選擇性別",'errMsg': "請先選擇目標性別，再執行此動作"}
+        self.select_count[1] = int(self.select_result_count_facial.currentText())
+        print(f'Send infomation:\n\tselect_sex={self.select_sex[1]}\n\tselect_count={self.select_count[1]}')
+        results = self.face_detector.find_specified_type_faces(self.select_sex[1], self.select_count[1])
+        return results
+    
+    def click_send(self) -> None:
+        """
+        送資料去處理
+        """
+        btn_name = self.sender().objectName()
+        print(f'{btn_name} press export')
+        if btn_name == 'commandLinkButton_send':
+            results = self._send_similarity_requires()
+        elif btn_name == 'commandLinkButton_send_facial':
+            results = self._send_facial_selection_requires()
+        else:
+            return
+        # show result
+        if isinstance(results, dict):
+            QtWidgets.QMessageBox.about(self, results['errTitle'], results['errMsg'])
+        elif len(results) == 0:
+            QtWidgets.QMessageBox.about(self, "唉呦", "沒有找到指定的目標五官")
+        else:
+            pass
 
     def click_export(self) -> None:
-        # 可以匯成檔案
-        print('press export')
-
+        """
+        可以匯成檔案
+        """
+        btn_name = self.sender().objectName()
+        print(f'{btn_name} press export')
+        # if btn_name == 'commandLinkButton_export':
+        #     pass
+        # elif btn_name == 'commandLinkButton_export_facial':
+        #     pass
     
     def __repr__(self):
-        return f'GUIController({self.select_sex},{self.target_img_path}, {self.select_count})'
-
+        return f'GUIController({self.select_count})'
 
     def __str__(self):
-        return f'GUIController(sex={self.select_sex},img_path={self.target_img_path}, count={self.select_count})'
+        return f'GUIController(count={self.select_count})'
