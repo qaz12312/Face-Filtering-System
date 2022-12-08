050f529d2c4e7c5c3a11b77ee8d72f2039da0e10
qaz12312 <watermoo22@gmail.com>
2022-12-09 01:53:15 +0800

    Update: add return values of functions in FaceController

--- controller/face.py
+++ controller/face.py
@@ -6,12 +6,22 @@ class FaceController(object):
         pass
 
     def extract_face(self, img_path:str)->tuple:
-        isSuccess, msg = face_obtain(img_path)
-        return isSuccess, msg
+        is_success, msg = face_obtain(img_path)
+        return is_success, msg
 
-    def find_similar_face(self, sex:int, count:int, img_path:list):
-        print(f'Get: sex={sex}, count={count}, img_paths={img_path}')
-        pass
+    def find_similar_faces(self, sex:int, count:int, img_path:list)->list:
+        """
+        查找相似的目標
+        """
+        print(f'[FaceController] Get: sex={sex}, count={count}, img_paths={img_path}')
+        return []
+
+    def find_specified_type_faces(self, sex:int, count:int)->list:
+        """
+        查找指定類型的面部
+        """
+        print(f'[FaceController] Get: sex={sex}, count={count}')
+        return []
 
     def __repr__(self):
         return f'FaceController()'
